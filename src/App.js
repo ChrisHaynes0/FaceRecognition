df410521abf0c45a65b7a7bda3f14d2f1328052c
ChrisHaynes0 <130589688+ChrisHaynes0@users.noreply.github.com>
2024-06-10 22:39:09 -0400

    made logo and imagelink form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
+import Particles from "react-particles";
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
+import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm'
+import Rank from './Components/Rank/Rank';
 import './App.css';
 
+
+const particlesOptions ={
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  }
+}
 class App extends Component {
   render() {
   return (
     <div className="App">
+    <Particles
+    params={particlesOptions}
+    />
       <Navigation />
       <Logo />
-      {/*<ImageLinkform /> 
-      <FaceRecognition />*/}
+      <Rank />
+      <ImageLinkForm /> 
+
+      {/*<FaceRecognition />*/}
     </div>
   );
 }
--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Tilt } from 'react-tilt';
+import brain from './brain.png';
 import './Logo.css';
 
 const Logo = () => {
     return (
         <div className="ma4 mt0">
-         <Tilt className= "Tilt br2 shadow-2" options={{ max: 25}} style={{ height: 250, width: 250 }}>
-      <div>👽</div>
+         <Tilt className= "Tilt br2 shadow-2" options={{ max: 25}} style={{ height: 70, width: 100 }}>
+      <div><img alt='logo' src={brain}/></div>
     </Tilt>
         </div>
     )
