5e6e88534ddbed1c891340faa2268b33201acce2
ChrisHaynes0 <130589688+ChrisHaynes0@users.noreply.github.com>
2024-06-19 21:49:51 -0400

    updating

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Particles from "react-particles";
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm'
@@ -7,28 +6,31 @@ import Rank from './Components/Rank/Rank';
 import './App.css';
 
 
-const particlesOptions ={
-  particles: {
-    line_linked: {
-      shadow: {
-        enable: true,
-        color: "#3CA9D1",
-        blur: 5
-      }
-    }
+class App extends Component {
+constructor() {
+  super();
+  this.state = {
+    input: '',
   }
 }
-class App extends Component {
+
+ onInputChange = (event) => {
+  console.log(event.target.value);
+ }
+
+ onButtonSubmit = () => {
+  console.log('click');
+ }
+
   render() {
   return (
     <div className="App">
-    <Particles
-    params={particlesOptions}
-    />
+
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm /> 
+      <ImageLinkForm onInputChange={this.onInputChange} 
+      onButtonSubmit={this.onButtonSubmit}/> 
 
       {/*<FaceRecognition />*/}
     </div>
